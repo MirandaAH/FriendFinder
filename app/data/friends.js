8016353ad6aa9e7daaa51910d9c31e61fb3e7a39
MirandaAH <hanemiranda@gmail.com>
2017-08-07 09:54:16 -0700

    update friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,34 @@
 
 var friends = [
   {
-    name: "Monica",
-    photo: "http://via.placeholder.com/350x150",
+    name: "JinYang",
+    photo: "https://pbs.twimg.com/profile_images/846510701195673600/N1Zmzqkn.jpg",
     scores: [2,3,2,2,1,5,3,2,1,2]
   },
   {
-    name: "Jeeeorge",
-    photo: "http://via.placeholder.com/350x150",
+    name: "Erlich Bachman",
+    photo: "http://www.piedpiper.com/app/themes/pied-piper/dist/images/erlich.png",
     scores: [3,4,2,3,1,2,3,5,4,3]
+  },
+  {
+    name: "Richard Hendricks",
+    photo: "http://www.piedpiper.com/app/themes/pied-piper/dist/images/richard.png",
+    scores: [1,2,4,3,4,2,1,3,5,3]
+  },
+  {
+    name: "Dinesh Chugtai",
+    photo: "http://www.piedpiper.com/app/themes/pied-piper/dist/images/dinesh.png",
+    scores: [2,3,4,1,2,3,4,5,4,3]
+  },
+  {
+    name: "Jared (DONALD) Dunn",
+    photo: "http://www.piedpiper.com/app/themes/pied-piper/dist/images/jared.png",
+    scores: [1,1,1,1,1,1,1,1,1,1]
+  },
+  {
+    name: "Bertram Gilfoyle",
+    photo: "http://www.piedpiper.com/app/themes/pied-piper/dist/images/gilfoyle.png",
+    scores: [5,5,5,5,5,5,5,5,5]
   }
 ]
 
