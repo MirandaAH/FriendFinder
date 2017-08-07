f5b6aaa929e604e26b7c90bb3baadd5d92e09982
MirandaAH <hanemiranda@gmail.com>
2017-08-06 21:08:21 -0700

    reworked app

---
+++ app/data/friends.js
@@ -0,0 +1,15 @@
+
+var friends = [
+  {
+    name: "Monica",
+    photo: "http://via.placeholder.com/350x150",
+    scores: [2,3,2,2,1,5,3,2,1,2]
+  },
+  {
+    name: "Jeeeorge",
+    photo: "http://via.placeholder.com/350x150",
+    scores: [3,4,2,3,1,2,3,5,4,3]
+  }
+]
+
+module.exports = friends;
