d348a1291ace6ec913c43691664cef89b93a1fb2
MirandaAH <hanemiranda@gmail.com>
2017-08-07 09:44:46 -0700

    update

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
